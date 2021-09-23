f4cac85aa04fc6d373165849c1784885945bc87d
Lan Liang <gcslyp@gmail.com>
2021-09-23 11:28:33 +0800

    Support http and kafka

---
+++ src/main/java/com/yunhorn/cloudevents/functions/CloudEventHttpFunction.java
@@ -0,0 +1,54 @@
+package com.yunhorn.cloudevents.functions;
+
+import java.net.URI;
+import org.apache.pulsar.functions.api.Context;
+import org.apache.pulsar.functions.api.Function;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * pulsar functions of http cloudevents
+ */
+public class CloudEventHttpFunction implements Function<byte[], byte[]> {
+
+    volatile RestTemplate  restTemplate;
+
+    @Override
+    public byte[] process(byte[] input, Context context) {
+
+        String target = context.getUserConfigMap().getOrDefault("target", "").toString();
+        context.getLogger().info("cloudevent.target:{}",target);
+        if(target.length()==0 || (!target.startsWith("http://") && !target.startsWith("https://"))){
+            return null;
+        }
+
+        if(restTemplate==null){
+            synchronized (restTemplate){
+                if(restTemplate==null){
+                    HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+                    httpRequestFactory.setConnectionRequestTimeout(Integer.valueOf(context.getUserConfigMap().getOrDefault("connectionRequestTime",3000).toString()));
+                    httpRequestFactory.setConnectTimeout(Integer.valueOf(context.getUserConfigMap().getOrDefault("connectTimeout",3000).toString()));
+                    httpRequestFactory.setReadTimeout(Integer.valueOf(context.getUserConfigMap().getOrDefault("readTimeout",3000).toString()));
+                    restTemplate = new RestTemplate(httpRequestFactory);
+                }
+            }
+        }
+
+
+        try {
+            restTemplate.exchange(RequestEntity.post(URI.create(target)) //
+                    .header("ce-id", context.getCurrentRecord().getProperties().getOrDefault("ceId", "ce-id")) //
+                    .header("ce-specversion",
+                            context.getCurrentRecord().getProperties().getOrDefault("ceSpecversion", "ceSpecversion")) //
+                    .header("ce-type", context.getCurrentRecord().getProperties().getOrDefault("ceType", "ceType")) //
+                    .header("ce-source", context.getCurrentRecord().getProperties().getOrDefault("ceSource", "ceSource")) //
+                    .contentType(MediaType.APPLICATION_JSON) //
+                    .body(new String(input)), String.class);
+        }catch (Exception e){
+            context.getLogger().error("push.cloudevent.failed!",e);
+        }
+        return input;
+    }
+}
---
+++ src/main/java/com/yunhorn/cloudevents/functions/CloudEventKafkaFunction.java
@@ -0,0 +1,88 @@
+package com.yunhorn.cloudevents.functions;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import io.cloudevents.core.message.Encoding;
+import io.cloudevents.jackson.JsonFormat;
+import io.cloudevents.kafka.CloudEventSerializer;
+import java.net.URI;
+import java.util.Properties;
+import java.util.UUID;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.pulsar.functions.api.Context;
+import org.apache.pulsar.functions.api.Function;
+import org.springframework.util.StringUtils;
+
+/**
+ * pulsar functions of kafka cloudevents
+ */
+public class CloudEventKafkaFunction implements Function<byte[], byte[]> {
+
+    KafkaProducer<String, CloudEvent> producer;
+
+    @Override
+    public byte[] process(byte[] input, Context context) {
+
+        String server = context.getUserConfigMap().getOrDefault("bootstrap.servers","").toString();
+        if(!StringUtils.hasText(server)){
+            return input;
+        }
+
+        if(producer==null){
+            synchronized (producer){
+                if(producer==null){
+                    // Basic producer configuration
+                    Properties props = new Properties();
+                    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+                    props.put(ProducerConfig.CLIENT_ID_CONFIG, context.getUserConfigMap().getOrDefault("client.id","cloudevent-kafka-producer").toString());
+                    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+                    // Configure the CloudEventSerializer to emit events as json structured events
+                    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CloudEventSerializer.class);
+                    props.put(CloudEventSerializer.ENCODING_CONFIG, Encoding.STRUCTURED);
+                    props.put(CloudEventSerializer.EVENT_FORMAT_CONFIG, JsonFormat.CONTENT_TYPE);
+
+                    // Create the KafkaProducer
+                    producer = new KafkaProducer<>(props);
+                }
+            }
+        }
+
+
+        String topic = context.getOutputTopic();
+
+        // Create an event template to set basic CloudEvent attributes
+        CloudEventBuilder eventTemplate = CloudEventBuilder.v1()
+                .withSource(URI.create(context.getUserConfigMap().getOrDefault("ce.source","cloudevent-source-kafka-producer").toString()))
+                .withType(context.getUserConfigMap().getOrDefault("ce.type","cloudevent-type-kafka-producer").toString());
+
+            try {
+                String id = context.getCurrentRecord().getProperties().getOrDefault("ce.id","");
+                if(id.length()==0){
+                    id = UUID.randomUUID().toString();
+                }
+
+                // Create the event starting from the template
+                CloudEvent event = eventTemplate.newBuilder()
+                        .withId(id)
+                        .withData("text/plain", input)
+                        .build();
+
+                // Send the record
+                RecordMetadata metadata = producer
+                        .send(new ProducerRecord<>(topic, id, event))
+                        .get();
+                context.getLogger().error("Record sent to partition " + metadata.partition() + " with offset " + metadata.offset());
+            } catch (Exception e) {
+                context.getLogger().error("Error while trying to send the record!",e);
+            }
+
+//        producer.flush();
+
+        return input;
+    }
+}
